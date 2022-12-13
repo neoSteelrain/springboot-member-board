f2f87b57c7f7eeb9761a4bce3fc96ccdbc551430
neoSteelrain <magnosix6@gmail.com>
2022-12-13 10:06:47 +0900

    build - 프로젝트 셋업:gitignore,db,application.yml 설정 추가

--- db-script/mysql.sql
+++ db-script/mysql.sql
@@ -1,3 +1,4 @@
+-- DB, 계정 생성
 create database db_springboot_member;
 create user 'user_springboot_member'@'localhost' identified by '1234';
 grant all privileges on db_springboot_member.* to 'user_springboot_member'@'localhost';
