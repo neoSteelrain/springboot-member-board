4b744be31fa8194e6011b5fc972feca924100f91
neoSteelrain <magnosix6@gmail.com>
2022-12-13 09:40:04 +0900

    init - 프로젝트 생성

---
+++ src/test/java/com/its/springboot/member/MemberBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.springboot.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
