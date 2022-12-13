0721b1692452983ea40d10847a17fc0d0341bbc2
neoSteelrain <magnosix6@gmail.com>
2022-12-13 10:01:12 +0900

    build - db, application.yml 설정 추가

---
+++ db-script/mysql.sql
@@ -0,0 +1,3 @@
+create database db_springboot_member;
+create user 'user_springboot_member'@'localhost' identified by '1234';
+grant all privileges on db_springboot_member.* to 'user_springboot_member'@'localhost';
